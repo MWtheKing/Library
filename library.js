6a90f106a79c310bea12ed579c60d85b13fa553f
MWtheKing <99815039+MWtheKing@users.noreply.github.com>
2025-09-26 12:38:59 -0400

    Add function that adds new created books in div
    When a book is created it is added to the bookGrid
    div in the html.

--- library.js
+++ library.js
@@ -8,17 +8,64 @@ function Book(author, title, pages, status) {
     this.status = status;
     this.info = function () {
         return `${this.title} by ${this.author}, ${pages}, ${status}`
-    } 
-}
+    };
+};
 
 function addBook(author, title, pages, status) {
         const book = new Book(author, title, pages, status);
         myLibrary.push(book)
         return book;
-    }
+    };
 
 
 addBook("MWTHEKING", "THE KING OF NOTHING", "100000", "Read")
-addBook("test", "test", "test", "test")
+addBook("Nobody", "Test", "0", "Not Read")
 
 console.table(myLibrary)
+
+function createCards() {
+
+    let books = myLibrary.length
+    let bookGrid = document.querySelector(".bookGrid")
+
+
+    myLibrary.forEach(book => {
+
+            let bookCard = document.createElement("div")
+            bookCard.classList.add("book");
+
+            let title = document.createElement("div")
+            title.classList.add("title");
+            title.textContent = book.title
+
+            let author = document.createElement("div")
+            author.classList.add("author");
+            author.textContent = `by ${book.author}`
+
+            let pages = document.createElement("div")
+            pages.classList.add("pages");
+            pages.textContent = `${book.pages} pages`
+
+            let status = document.createElement("div")
+            status.classList.add("status");
+            status.textContent = book.status
+
+            bookCard.appendChild(title)
+            bookCard.appendChild(author)
+            bookCard.appendChild(pages)
+            bookCard.appendChild(status)
+
+
+            bookGrid.appendChild(bookCard)
+    });
+};
+
+function newBookForm() {
+    
+}
+
+
+createCards();
+
+
+
