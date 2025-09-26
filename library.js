3268e103c2d2793a33e3a5ed2517776ed44e8694
MWtheKing <99815039+MWtheKing@users.noreply.github.com>
2025-09-26 10:09:35 -0400

    Add book function and addBook function
    addBook function uses book constuctor to add a
    new book to myLibrary array

--- library.js
+++ library.js
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+const myLibrary = [];
+
+function Book(author, title, pages, status) {
+    this.id = crypto.randomUUID()
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.status = status;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${pages}, ${status}`
+    } 
+}
+
+function addBook(author, title, pages, status) {
+        const book = new Book(author, title, pages, status);
+        myLibrary.push(book)
+        return book;
+    }
+
+
+addBook("MWTHEKING", "THE KING OF NOTHING", "100000", "Read")
+addBook("test", "test", "test", "test")
+
+console.table(myLibrary)
